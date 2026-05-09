04d894f1702864f67a2b491452808283b82a2c80
Sudharshan Acharya <sudharshan6acharya@gmail.com>
2026-05-09 10:21:08 +0530

    .gitignore updated

--- lib/Socket/config.dart
+++ lib/Socket/config.dart
@@ -1,4 +1,3 @@
 class Config {
-  // static const String uri = "https://game-score-board-backend.fly.dev/";
-  static const String uri = "https://feb6-122-171-22-128.ngrok-free.app";
+  static const String uri = "https://game-score-board-backend.fly.dev/";
 }
